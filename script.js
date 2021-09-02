d1c7c4c5f3f9b797572ea4ad5b15daf6ee3ee47a
santonu-ahmed <santonu974@gmail.com>
2021-09-03 00:01:53 +0600

    sum changes uwu

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const getSfw = document.getElementById('sfw');
 const getNsfw = document.getElementById('nsfw');
+const spinner = document.getElementById('spinner');
+const imgbtn = document.getElementById('waifu-img');
 
+spinner.classList.add('d-none')
 getSfw.addEventListener('click', function(){
     const apisfw = 'https://api.waifu.pics/sfw/cry';
+    spinner.classList.remove('d-none');
+    imgbtn.classList.add('d-none');
     fetch(apisfw)
         .then(response => response.json())
         .then(waifuImage => {
@@ -10,6 +15,9 @@ getSfw.addEventListener('click', function(){
 
             const imgUrl = waifuImage.files;
             document.getElementById('waifu-img').src = `${ waifuImage.url }`;
+        })
+        .finally(() =>{
+            spinner.classList.add('d-none')
         })
 })
 
